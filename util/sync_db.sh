522301719838dfa3577a85f629b4e502c24111df
P4SSER8Y <P4SSER8Y@outlook.com>
2019-03-08 23:19:40 +0800

    bug fixed

--- util/sync_db.sh
+++ util/sync_db.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 tmp_db="/tmp/home-assistant-dump.db"
-backup_db="$(cd `dirname $0`; pwd)/backup.db"
+backup_db="$(cd `dirname $0`; pwd)/../backup.db"
 
 if [ -f "$tmp_db" ]; then
     rm $tmp_db
