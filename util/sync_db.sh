91c694a9091576c66213da7db568c47b881a7066
P4SSER8Y <P4SSER8Y@outlook.com>
2019-03-13 11:48:56 +0800

    little change

--- util/sync_db.sh
+++ util/sync_db.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+src_db="/tmp/home-assistant.db"
 tmp_db="/tmp/home-assistant-dump.db"
 backup_db="$(cd `dirname $0`; pwd)/../backup.db"
 
@@ -7,7 +8,7 @@ if [ -f "$tmp_db" ]; then
     rm $tmp_db
 fi
 
-cp /tmp/home-assistant.db $tmp_db
+cp $src_db $tmp_db
 sqlite3 $backup_db "ATTACH '$tmp_db' AS src; INSERT OR IGNORE INTO states SELECT * FROM src.states;"
 rm $tmp_db
 
